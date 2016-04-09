4b693dbea4ca8c147f6ade7b78f6f8a844f9f340
ozgur sahin <ozgursahin555@gmail.com>
2016-04-09 22:40:49 +0200

    First commit

--- Donate/PaypalInterface.m
+++ Donate/PaypalInterface.m
@@ -15,6 +15,6 @@
 -(void)initPaypal
 {
     [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction : @"YOUR_CLIENT_ID_FOR_PRODUCTION",
-                                                           PayPalEnvironmentSandbox : @"AbawWOa0G-aY6CWRsVNrtndgQ4AfpyfF9nTk1LvzK1VCPNVcI-rvErHIu8gaAlkZ4MpW5P7Hx1ZnJz_g"}];
+                                                           PayPalEnvironmentSandbox : @"YOUR_CLIENT_ID_FOR_SANDBOX"}];
 }
 @end
